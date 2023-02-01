5e6c541abdc4c9a3cd498e1b0f43882c29b8d140
Gessio Mori <gessiomori@protonmail.com>
2023-02-01 16:28:10 -0300

    changes next config and implements home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import type { AppProps } from "next/app";
+import GlobalStyle from "@/styles/global";
+import { ThemeProvider } from "styled-components";
+import { theme } from "@/styles/theme";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </ThemeProvider>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { Cart } from "@/components/Cart";
+import { Footer } from "@/components/Footer";
+import { Navbar } from "@/components/Navbar";
+import { Products } from "@/components/Products";
+import { AppContainer } from "@/styles/components/Container";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <AppContainer>
+      <Cart />
+      <Navbar />
+      <Products />
+      <Footer />
+    </AppContainer>
   );
 }
